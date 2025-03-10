d9e2026e72253300de41e5ddec8dd3e792b29d97
Max <maxkolchin@yahoo.com>
2025-03-10 19:14:54 +0300

    use TestCaseSource

--- PowerOfAttornyApp.Service.Tests/PowerOfAttornyBuilderTest.cs
+++ PowerOfAttornyApp.Service.Tests/PowerOfAttornyBuilderTest.cs
@@ -49,28 +49,29 @@ namespace PowerOfAttornyApp.Service.Tests
 			Assert.AreEqual(expected, actual);
 		}
 
-		[Test]
-		public void Create_SomeNotRandom_ReturnsCorrect()
+		[TestCase(123, "Vasily", "Ivanov", 1999, "Ivanov Vasily", 456, "Russia", "Omsk", "Lenina", "22a", "2100.02.03", $"Russia, Omsk, Lenina, 22a")]
+		[TestCase(234, "Ivan", "Petrov", 2001, "Petrov Ivan", 567, "Ukraine", "Donetsk", "Bandery", "33b", "1456.04.05", $"Ukraine, Donetsk, Bandery, 33b")]
+		public void Create_SomeNotRandom_ReturnsCorrect(int personId, string personName, string personLastName, int personBirthYear, string expectedFullName,
+			int addressId, string addressCountry, string addressCity, string addressStreet, string addressHouse, string expirationDateStr, string expectedFullAddress)
 		{
 			// arrange
-			var person = new Person(123, "Vasily", "Ivanov", 1999);
-			var address = new Address(456, "Russia", "Omsk", "Lenina", "22a");
-			DateTime expirationDate = new DateTime(2100, 2, 3);
+			var person = new Person(personId, personName, personLastName, 1999);
+			var address = new Address(addressId, addressCountry, addressCity, addressStreet, addressHouse);
+			DateTime expirationDate = DateTime.ParseExact(expirationDateStr, "yyyy.MM.dd", null);
 
 			var expected = new PowerOfAttorny(
-				123, 
-				"Vasily", 
-				"Ivanov", 
-				1999,
-				"Ivanov Vasily",
+				personId, 
+				personName, 
+				personLastName, 
+				personBirthYear,
+				expectedFullName,
 
-				456, 
-				"Russia", 
-				"Omsk", 
-				"Lenina", 
-				"22a", 
-				
-				$"Russia, Omsk, Lenina, 22a",
+				addressId, 
+				addressCountry, 
+				addressCity, 
+				addressStreet, 
+				addressHouse, 
+				expectedFullAddress,
 
 				expirationDate);
 
--- PowerOfAttornyApp.Service.Tests/PowerOfAttornyServiceTest.cs
+++ PowerOfAttornyApp.Service.Tests/PowerOfAttornyServiceTest.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using Moq;
 using AutoFixture;
 using PowerOfAttornyApp.Service.Entities;
+using System.Linq.Expressions;
 
 namespace PowerOfAttornyApp.Service.Tests
 {
@@ -32,27 +33,24 @@ namespace PowerOfAttornyApp.Service.Tests
 			_builder.VerifyAll();
 		}
 
-		[Test]
-		public void CreatePowerOfAttorny_2000_Kiev_CallsCorrect()
+		[TestCaseSource(nameof(Cases))]
+		public void CreatePowerOfAttorny_2000_Kiev_CallsCorrect(string city, int birthYear, DateTime expectedExpirationDate,
+			 PublishMethodProvider regMethodProvider, PublishMethodProvider fundMethodProvider)
 		{
 			// arrange
 			string snilsNumber = _fixture.Create<string>();
-
-			int birthYear = 2000;
-			string city = "Kiev";
 
 			var person = _fixture.Build<Person>().With(p => p.BirthYear, birthYear).Create();
 			_dal.Setup(d => d.GetPerson(snilsNumber)).Returns(person);
 
 			var address = _fixture.Build<Address>().With(a => a.City, city).Create();
 			_dal.Setup(d => d.GetPersonRegistrationAddress(person.Id)).Returns(address);
-
-			var expirationDate = new DateTime(2030, 1, 1);						
+					
 			var document = _fixture.Create<PowerOfAttorny>();
-			_builder.Setup(b => b.Create(person, address, expirationDate)).Returns(document);
+			_builder.Setup(b => b.Create(person, address, expectedExpirationDate)).Returns(document);
 				
-			_publisher.Setup(d => d.PublishToNonMoscowRegistry(document)).Verifiable();
-			_publisher.Setup(d => d.PublishToPensionFund(document)).Verifiable();
+			_publisher.Setup(regMethodProvider(document)).Verifiable();
+			_publisher.Setup(fundMethodProvider(document)).Verifiable();
 			_dal.Setup(d => d.AddPowerOfAttorny(document)).Verifiable();
 
 			// act
@@ -63,97 +61,17 @@ namespace PowerOfAttornyApp.Service.Tests
 			VerifyAllMocks();
 		}
 
-		[Test]
-		public void CreatePowerOfAttorny_2000_Moscow_CallsCorrect()
+		public delegate Expression<Action<IPowerOfAttornyPublisher>> PublishMethodProvider(PowerOfAttorny document);
+		public static object[] Cases =
 		{
-			// arrange
-			string snilsNumber = _fixture.Create<string>();
-
-			int birthYear = 2000;
-			string city = "Moscow";
-
-			var person = _fixture.Build<Person>().With(p => p.BirthYear, birthYear).Create();
-			_dal.Setup(d => d.GetPerson(snilsNumber)).Returns(person);
-
-			var address = _fixture.Build<Address>().With(a => a.City, city).Create();
-			_dal.Setup(d => d.GetPersonRegistrationAddress(person.Id)).Returns(address);
-
-			var expirationDate = new DateTime(2040, 1, 1);						
-			var document = _fixture.Create<PowerOfAttorny>();
-			_builder.Setup(b => b.Create(person, address, expirationDate)).Returns(document);
-				
-			_publisher.Setup(d => d.PublishToMoscowRegistry(document)).Verifiable();
-			_publisher.Setup(d => d.PublishToPensionFund(document)).Verifiable();
-			_dal.Setup(d => d.AddPowerOfAttorny(document)).Verifiable();
-
-			// act
-			var actual = _target.CreatePowerOfAttorny(snilsNumber);
-
-			// assert
-			Assert.AreEqual(document, actual);
-			VerifyAllMocks();
-		}
-
-		[Test]
-		public void CreatePowerOfAttorny_2001_Kiev_CallsCorrect()
-		{
-			// arrange
-			string snilsNumber = _fixture.Create<string>();
-
-			int birthYear = 2001;
-			string city = "Kiev";
-
-			var person = _fixture.Build<Person>().With(p => p.BirthYear, birthYear).Create();
-			_dal.Setup(d => d.GetPerson(snilsNumber)).Returns(person);
-
-			var address = _fixture.Build<Address>().With(a => a.City, city).Create();
-			_dal.Setup(d => d.GetPersonRegistrationAddress(person.Id)).Returns(address);
-
-			var expirationDate = new DateTime(2031, 1, 1);					
-			var document = _fixture.Create<PowerOfAttorny>();
-			_builder.Setup(b => b.Create(person, address, expirationDate)).Returns(document);
-				
-			_publisher.Setup(d => d.PublishToNonMoscowRegistry(document)).Verifiable();
-			_publisher.Setup(d => d.PublishToUniversityFund(document)).Verifiable();
-			_dal.Setup(d => d.AddPowerOfAttorny(document)).Verifiable();
-
-			// act
-			var actual = _target.CreatePowerOfAttorny(snilsNumber);
-
-			// assert
-			Assert.AreEqual(document, actual);
-			VerifyAllMocks();
-		}
-
-		[Test]
-		public void CreatePowerOfAttorny_2001_Moscow_CallsCorrect()
-		{
-			// arrange
-			string snilsNumber = _fixture.Create<string>();
-
-			int birthYear = 2001;
-			string city = "Moscow";
-
-			var person = _fixture.Build<Person>().With(p => p.BirthYear, birthYear).Create();
-			_dal.Setup(d => d.GetPerson(snilsNumber)).Returns(person);
-
-			var address = _fixture.Build<Address>().With(a => a.City, city).Create();
-			_dal.Setup(d => d.GetPersonRegistrationAddress(person.Id)).Returns(address);
-
-			var expirationDate = new DateTime(2041, 1, 1);						
-			var document = _fixture.Create<PowerOfAttorny>();
-			_builder.Setup(b => b.Create(person, address, expirationDate)).Returns(document);
-				
-			_publisher.Setup(d => d.PublishToMoscowRegistry(document)).Verifiable();
-			_publisher.Setup(d => d.PublishToUniversityFund(document)).Verifiable();
-			_dal.Setup(d => d.AddPowerOfAttorny(document)).Verifiable();
-
-			// act
-			var actual = _target.CreatePowerOfAttorny(snilsNumber);
-
-			// assert
-			Assert.AreEqual(document, actual);
-			VerifyAllMocks();
-		}
+			new object[] { "Kiev", 2000, new DateTime(2030, 1, 1), 
+				(PublishMethodProvider)(d => p => p.PublishToNonMoscowRegistry(d)), (PublishMethodProvider)(d => p => p.PublishToPensionFund(d)) },
+			new object[] { "Moscow", 2000, new DateTime(2040, 1, 1), 
+				(PublishMethodProvider)(d => p => p.PublishToMoscowRegistry(d)), (PublishMethodProvider)(d => p => p.PublishToPensionFund(d)) },
+			new object[] { "Kiev", 2001, new DateTime(2031, 1, 1),
+				(PublishMethodProvider)(d => p => p.PublishToNonMoscowRegistry(d)), (PublishMethodProvider)(d => p => p.PublishToUniversityFund(d)) },
+			new object[] { "Moscow", 2001, new DateTime(2041, 1, 1),
+				(PublishMethodProvider)(d => p => p.PublishToMoscowRegistry(d)), (PublishMethodProvider)(d => p => p.PublishToUniversityFund(d)) },
+		};
 	}
 }
